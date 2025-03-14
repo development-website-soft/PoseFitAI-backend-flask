c566443fc54bc4d60c1f8dee73e22c2e9e74ded6
BrightCodersApp <hhhusamm777056@gmail.com>
2025-03-14 04:14:36 +0300

    backend commit

---
+++ run.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from app import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run()
+
+# fa43da3d-e2e6-04f9-4551-33143c0353b5
+
+# auth
+# HRKU-5eaee473-9e4a-4f08-a3d8-0afec07cfb05
